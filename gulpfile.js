255287a374de7ec0c301413438ca072fab177820
James Calcaben <jcalcaben@magento.com>
2018-06-18 10:06:30 -0500

    Separate preset style from the gulpfile and into a new module

--- gulpfile.js
+++ gulpfile.js
@@ -13,39 +13,8 @@ var contentDirectories = [
     "venia-pwa-concept",
 ];
 
+var styles = require('./modules/remark-preset-lint-devdocs');
 
-// remark-lint style rules: https://github.com/remarkjs/remark-lint/blob/master/doc/rules.md#list-of-rules
-var styles = [
-    require('remark-lint'),
-    [require('remark-lint-blockquote-indentation'), ['error', 2] ],
-    [require('remark-lint-code-block-style'), ['error', 'fenced'] ],
-    [require('remark-lint-emphasis-marker'), ['error', '_'] ],
-    [require('./modules/remark-lint-no-undefined-external-references'), ['error'] ], //Custom implementation for checking jekyll-style links
-    [require('remark-lint-no-unused-definitions'), ['error'] ], //Needs a custom implementation of this to check for jekyll-style links
-    [require('remark-lint-no-reference-like-url'), ['error'] ],
-    [require('remark-lint-no-tabs'), ['error'] ],
-    [require('remark-lint-final-newline'), ['error']],
-    [require('remark-lint-first-heading-level'), ['error', 2]],
-    [require('remark-lint-hard-break-spaces'), ['error']],
-    [require('remark-lint-heading-increment'), ['error']],
-    [require('remark-lint-list-item-content-indent'), ['error']],
-    [require('remark-lint-no-consecutive-blank-lines'), ['error']],
-    [require('remark-lint-no-duplicate-headings'), ['error']],
-    [require('remark-lint-no-file-name-consecutive-dashes'), ['error']],
-    [require('remark-lint-no-file-name-irregular-characters'), ['error', /\.a-z0-9-/ ]],
-    [require('remark-lint-no-file-name-outer-dashes'), ['error']],
-    [require('remark-lint-no-heading-content-indent'), ['error']],
-    [require('remark-lint-no-heading-like-paragraph'), ['error']],
-    [require('remark-lint-no-heading-punctuation'), ['error']],
-    [require('remark-lint-no-inline-padding'), ['error']],
-    [require('remark-lint-no-missing-blank-lines'), ['error']],
-    [require('remark-lint-no-paragraph-content-indent'), ['error']],
-    [require('remark-lint-no-shell-dollars'), ['error']],
-    [require('remark-lint-ordered-list-marker-style'), ['error','.']],
-    [require('remark-lint-strong-marker'), ['error','*']],
-    [require('remark-lint-table-pipe-alignment'), ['error']],
-    [require('remark-lint-unordered-list-marker-style'), ['error', '*']]
-]
 var styleChecker = (filename) => {
     let StyleChecker = require('./modules/StyleChecker');
 
---
+++ modules/remark-preset-lint-devdocs.js
@@ -0,0 +1,39 @@
+/**
+ * remark preset to configure remark-lint with settings that enforce devdocs styles
+ * 
+ * remark-lint style rules: https://github.com/remarkjs/remark-lint/blob/master/doc/rules.md#list-of-rules
+ */
+'use strict'
+
+module.exports = [
+    require('remark-lint'),
+    [require('remark-lint-blockquote-indentation'), ['error', 2] ],
+    [require('remark-lint-code-block-style'), ['error', 'fenced'] ],
+    [require('remark-lint-emphasis-marker'), ['error', '_'] ],
+    [require('remark-lint-no-unused-definitions'), ['error'] ], //Needs a custom implementation of this to check for jekyll-style links
+    [require('remark-lint-no-reference-like-url'), ['error'] ],
+    [require('remark-lint-no-tabs'), ['error'] ],
+    [require('remark-lint-final-newline'), ['error']],
+    [require('remark-lint-first-heading-level'), ['error', 2]],
+    [require('remark-lint-hard-break-spaces'), ['error']],
+    [require('remark-lint-heading-increment'), ['error']],
+    [require('remark-lint-list-item-content-indent'), ['error']],
+    [require('remark-lint-no-consecutive-blank-lines'), ['error']],
+    [require('remark-lint-no-duplicate-headings'), ['error']],
+    [require('remark-lint-no-file-name-consecutive-dashes'), ['error']],
+    [require('remark-lint-no-file-name-irregular-characters'), ['error', /\.a-z0-9-/ ]],
+    [require('remark-lint-no-file-name-outer-dashes'), ['error']],
+    [require('remark-lint-no-heading-content-indent'), ['error']],
+    [require('remark-lint-no-heading-like-paragraph'), ['error']],
+    [require('remark-lint-no-heading-punctuation'), ['error']],
+    [require('remark-lint-no-inline-padding'), ['error']],
+    [require('remark-lint-no-missing-blank-lines'), ['error']],
+    [require('remark-lint-no-paragraph-content-indent'), ['error']],
+    [require('remark-lint-no-shell-dollars'), ['error']],
+    [require('remark-lint-ordered-list-marker-style'), ['error','.']],
+    [require('remark-lint-strong-marker'), ['error','*']],
+    [require('remark-lint-table-pipe-alignment'), ['error']],
+    [require('remark-lint-unordered-list-marker-style'), ['error', '*']],
+
+    [require('./remark-lint-no-undefined-external-references'), ['error'] ] //Custom implementation for checking jekyll-style links
+]
